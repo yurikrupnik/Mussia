6962be559458d3f32198b2b929188ba61baf20f6
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-01 21:05:02 +0200

    render error.ejs onn 404

--- server/routes/error/controller.js
+++ server/routes/error/controller.js
@@ -1,5 +1,5 @@
 import path from 'path';
 import {root} from '../../config/env';
 export default (req, res) => {
-    res.status(404).sendFile(path.join(root, 'views/404.html'));
+    res.status(404).render('error');
 };
