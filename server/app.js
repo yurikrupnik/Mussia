4bfe7af8c15de5a8477e167421184b804982c23e
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-02 11:33:30 +0200

    added cookie parser but not setting cookies yet added run file timeout of requests is 4000ms now app set title to mussia

--- server/app.js
+++ server/app.js
@@ -6,6 +6,12 @@ import bodyParser from './services/middlewares/bodyParser';
 import views from './services/middlewares/views';
 import './config/env/db'; // just loading it for connection
 
+import cookieParser from 'cookie-parser';
+
+app.set('title', 'Mussia');
+app.get('title'); // "My Site"
+// app.name = 'Mussia';
+app.use(cookieParser());
 // views middleware
 views(app);
 // statics
@@ -13,19 +19,26 @@ app.use(express.static('client/public')); // must be before router
 // webpack middleware
 webpack(app); // test
 bodyParser(app); // test
-
 // demo for middlewares
 /*let logger = (req, res, next) => {
-    console.log('body 1');
-    next();
-};
-app.post('/users', logger);
-app.use((req, res, next) => {
-    console.log('req.url', req.url);
-    next();
-});*/
-
+ console.log('body 1');
+ next();
+ };
+ app.post('/users', logger);
+ app.use((req, res, next) => {
+ console.log('req.url', req.url);
+ next();
+ });*/
+app.use(function (req, res, next) {
+    console.log('cookies', req.cookies);
+    next()
+});
 // router middleware
 router(app); // test
 
+// cookies log works but no cookies set yet
+// app.use(function (req, res, next) {
+//     console.log('cookies', req.cookies);
+//     next()
+// });
 export default app;
