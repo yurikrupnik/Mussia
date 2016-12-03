89b77fe5c027f2215c8f7ee020b7f9099e3b0c40
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-03 02:42:43 +0200

    fixed users route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 import root, {rootUrl} from './root';
-import users from './users';
+import users, {usersUrl} from './users';
 import payments, {paymentsUrl} from '../api/payments';
 import friends, {friendsUrl} from '../api/friends';
 
 export default (app) => {
     app.use(rootUrl, root);
-    app.use('/', users); // todo make same as db routes
+    app.use(usersUrl, users); // todo make same as db routes
     app.use(paymentsUrl, payments);
     app.use(friendsUrl, friends);
 }
