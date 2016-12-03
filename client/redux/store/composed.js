e102cd653b5ee617abcb1d3641aad5d197bba8b7
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-03 15:42:43 +0200

    removed useless redux packages

--- client/redux/store/composed.js
+++ client/redux/store/composed.js
@@ -1,10 +1,6 @@
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
 import {applyMiddleware, compose} from 'redux';
-
-const logger = createLogger();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default composeEnhancers(
     // applyMiddleware(thunk, promise, logger),
 );
