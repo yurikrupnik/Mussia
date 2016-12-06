7cb7a559d6b5008f911434d14715086aed7b95f5
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-06 02:00:46 +0200

    users api folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
 import root, {rootUrl} from './root';
 import register, {registerUrl} from './register';
-import users, {usersUrl} from './users';
+import users, {usersUrl} from '../api/users';
 import payments, {paymentsUrl} from '../api/payments';
 import friends, {friendsUrl} from '../api/friends';
 
 export default (app) => {
     app.use(rootUrl, root);
-    app.use(usersUrl, users); // todo make same as db routes
-    app.use(registerUrl, register); // todo make same as db routes
+    app.use(usersUrl, users);
+    app.use(registerUrl, register);
     app.use(paymentsUrl, payments);
     app.use(friendsUrl, friends);
 }
--- server/routes/register/controller.js
+++ server/routes/register/controller.js
@@ -1,3 +1,6 @@
 export let handlePost = (req, res) => {
+    let body = req.body;
+    console.log('body', body);
+
     res.json([1, 23, 4]);
 };
