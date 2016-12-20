ed0649f08e8021cc4ecc50fe4e2cc9fbeb895a2b
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-20 22:23:45 +0200

    fixed root route - after api routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,9 @@ import payments, {paymentsUrl} from '../api/payments';
 import friends, {friendsUrl} from '../api/friends';
 
 export default (app) => {
-    app.use(rootUrl, root);
     app.use(usersUrl, users);
     app.use(registerUrl, register);
     app.use(paymentsUrl, payments);
     app.use(friendsUrl, friends);
+    app.use(rootUrl, root); // after api calls to handle any other request that match *
 }
