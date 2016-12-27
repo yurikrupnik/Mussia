8758f0ba8737f14f64cc47a3fcd5e58b236d8245
yurikrupnik <krupnik.yuri@gmail.com>
2016-12-27 21:50:19 +0200

    redux working

--- app/redux/store/composed.js
+++ app/redux/store/composed.js
@@ -1,5 +1,5 @@
 import {compose} from 'redux';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composeEnhancers = global.window && global.window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default composeEnhancers();
-    // applyMiddleware(thunk, promise, logger),
--- app/redux/store/store.js
+++ app/redux/store/store.js
@@ -1,7 +1,6 @@
 import {createStore} from 'redux';
 import reducer from '../reducers/index';
-// import composed from './composed';
+import composed from './composed';
 
-// export default createStore(reducer, {}, composed); // client
-export default createStore(reducer) // server
+export default createStore(reducer, {}, composed);
 
