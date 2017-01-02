f8e5acc28f29ee9fa06359ad68dc5cd5efedbc1f
yurikrupnik <krupnik.yuri@gmail.com>
2017-01-02 03:06:35 +0200

    redux works on server

--- app/redux/store/wrapper.js
+++ app/redux/store/wrapper.js
@@ -4,16 +4,7 @@ import configureStore from './store';
 import Material from '../../Wrappers/Material';
 
 export default ({initialState}) => {
-    console.log('initialState', initialState);
-    
     let store = configureStore(initialState);
-
-    console.log('store', store);
-
-    let state = store.getState();
-
-    console.log('state', state);
-
     return (
         <Provider store={store}>
             <Material />
