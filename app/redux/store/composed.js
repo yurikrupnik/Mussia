48c2a69de1d6f310e4959a83ac94e85f500eed2e
yurikrupnik <krupnik.yuri@gmail.com>
2017-01-03 14:38:52 +0200

    redux works on server with trunk for assync actions

--- app/redux/store/composed.js
+++ app/redux/store/composed.js
@@ -1,4 +1,6 @@
-import {compose} from 'redux';
+import {compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 const composeEnhancers = global.window && global.window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default composeEnhancers();
+export default composeEnhancers(
+    applyMiddleware(thunk)
+);
