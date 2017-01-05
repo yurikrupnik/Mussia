a7811b846fd2758088604868fd90632a89ed2115
yurikrupnik <krupnik.yuri@gmail.com>
2017-01-06 01:55:19 +0200

    small refactor

--- app/Router/middleware.js
+++ app/Router/middleware.js
@@ -1,26 +1,52 @@
 import React, {Component, PropTypes} from 'react';
-import {routes} from './rootRoute';
-import {match} from 'react-router'
-import {renderHtml} from './render';
+// import {routes} from './rootRoute';
+// import {match} from 'react-router'
+import {handleRoutesMiddlewares} from './render';
 
+// let handleState = (req, res) => {
+//     let store = configureStore();
+//     res.locals.state = store.getState();
+// };
 
-let handleRoutes = (req, res) => {
-    match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
-        if (error) {
-            res.status(500).send(error.message);
-        } else if (redirectLocation) {
-            res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-        } else if (renderProps) {
-            // You can also check renderProps.components or renderProps.routes for
-            // your "not found" component or route respectively, and send a 404 as
-            // below, if you're using a catch-all route.
-            renderHtml(renderProps, res);
-        } else {
-            res.status(404).send('Not found')
-        }
-    });
-};
+// let renderServer = (req, res) => {
+//     res.status(200);
+//     res.render('index');
+// };
+
+// let renderHtml = (props, response) => {
+//     let app = renderToString(
+//         <AppWrapper state={{}} {...props}/>
+//     );
+//
+//     let store = configureStore();
+//     state = store.getState();
+//     console.log('state', state);
+//
+//     response.locals = {app, state};
+//     // response.status(200);
+//     // response.render('index');
+//     // renderServer();
+// };
+// let handleRoutes = (req, res, next) => {
+//     match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
+//         if (error) {
+//             res.status(500).send(error.message);
+//         } else if (redirectLocation) {
+//             res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+//         } else if (renderProps) {
+//             // You can also check renderProps.components or renderProps.routes for
+//             // your "not found" component or route respectively, and send a 404 as
+//             // below, if you're using a catch-all route.
+//
+//             res.locals.app = foundMath(renderProps);
+//             // renderHtml(renderProps, res);
+//             next();
+//         } else {
+//             res.status(404).render('error');
+//         }
+//     });
+// };
 
 export default (app) => {
-    app.get('*', handleRoutes);
+    app.get('*', handleRoutesMiddlewares);
 }
--- app/Router/render.js
+++ app/Router/render.js
@@ -4,27 +4,48 @@ import AppWrapper from '../Wrappers/App';
 import {renderToString} from 'react-dom/server';
 import {render} from 'react-dom';
 
-let state;
 
-let renderServer = (res) => {
+
+import {routes} from './rootRoute';
+import {match} from 'react-router'
+
+
+let preRenderString = (props) => renderToString(<AppWrapper state={{}} {...props}/>);
+
+// middlewares
+let handleState = (req, res, next) => {
+    let store = configureStore();
+    res.locals.state = store.getState();
+    next();
+};
+
+
+let renderServer = (req, res) => {
     res.status(200);
     res.render('index');
 };
 
-let renderHtml = (props, response) => {
-    let app = renderToString(
-        <AppWrapper state={{}} {...props}/>
-    );
 
-    let store = configureStore();
-    state = store.getState();
-    console.log('state', state);
-
-    response.locals = {app, state};
-    response.status(200);
-    response.render('index');
-    // renderServer(response);
+let matchRoutes = (req, res, next) => {
+    match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
+        if (error) {
+            res.status(500).send(error.message);
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+        } else if (renderProps) {
+            res.locals.app = preRenderString(renderProps);
+            next();
+        } else {
+            res.status(404).render('error');
+        }
+    });
 };
+
+let handleRoutesMiddlewares = [
+    matchRoutes,
+    handleState,
+    renderServer
+];
 
 let renderToDOM = (initialState = window.__PRELOADED_STATE__) => {
     render(<AppWrapper state={initialState} />, document.getElementById('root'));
@@ -32,6 +53,6 @@ let renderToDOM = (initialState = window.__PRELOADED_STATE__) => {
 
 
 export {
-    renderHtml,
+    handleRoutesMiddlewares,
     renderToDOM
 }
