7b20f7d5ec9219537f18802fbceb68ff55c7939e
yurikrupnik <krupnik.yuri@gmail.com>
2017-01-14 02:32:37 +0200

    removed secret from git

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-// import Root from '../Wrappers/Root/Root';
-// import paymentsRoute from './payments';
-// import counterRoute from './couter'
-// import countersRoute from './counters';
-// {/*let Pay = () => (<div>pay bitch</div>);*/}
-// let Payments = () => (<div>Payments bitch</div>);
 
 if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require);
 
@@ -15,7 +8,7 @@ export let routes = {
     getComponents(state, cb) {
         // console.log('state in root route', state);
         // do asynchronous stuff to find the components
-        cb(null, require('../Wrappers/Root/Root'))
+        cb(null, require('../Wrappers/Root/Root'));
     },
     getChildRoutes(location, cb) {
         require.ensure([], function() {
@@ -28,7 +21,7 @@ export let routes = {
         });
     },
     getIndexRoute(partialNextState, callback) {
-        require.ensure([], function (require) {
+        require.ensure([], function () {
             callback(null, {
                 component: require('../Wrappers/Dashboard'),
             })
