9b398afdcbd28be0e677be07a82a628c2c05323b
yurikrupnik <krupnik.yuri@gmail.com>
2017-03-19 02:53:00 +0200

    sync bcrypt cleaner

--- app/config/auth.js
+++ app/config/auth.js
@@ -5,7 +5,7 @@ import Users from '../api/users/model';
 
 import bcrypt from 'bcrypt';
 
-var secrets = require('./secrets.json');
+let secrets = require('./secrets.json');
 
 let serialize = (user, done) => done(null, user.id);
 let deserialize = (id, done) => Users.findOne({id}, done);
@@ -16,7 +16,6 @@ const localStrategyParameters = {
 };
 
 let localStrategy = (req, email, password, done) => {
-
     Users.findOne({email})
         .then((user) => {
             if (!user) {
@@ -36,6 +35,9 @@ let localStrategy = (req, email, password, done) => {
         .catch((err)=> done(err));
 };
 
+function local() {
+    return new LocalStrategy(localStrategyParameters, localStrategy);
+}
 
 function generateHash(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10)); // todo not sync
@@ -53,44 +55,26 @@ export default (passport) => {
 
     // local
 
-    passport.use(new LocalStrategy(localStrategyParameters, localStrategy));
+    passport.use(local());
     // failure with github register
     passport.use(new GithubStrategy(secrets.github,
         function (token, refreshToken, profile, done) {
             // asynchronous
             process.nextTick(function () {
-
                 // find the user in the database based on their facebook id
-                Users.findOne({'id': profile.id}, function (err, user) {
+                Users.findOne({'id': profile.id})
+                    .then(function (user) {
+                        // if the user is found, then log them in
+                        if (user) {
+                            done(null, user); // user found, return that user
+                        } else {
+                            // save our user to the database
+                            Users.insert(profile._json).then((newUs) => done(null, newUs));
+                        }
 
-                    // if there is an error, stop everything and return that
-                    // ie an error connecting to the database
-                    if (err)
-                        return done(err);
-
-                    // if the user is found, then log them in
-                    if (user) {
-                        return done(null, user); // user found, return that user
-                    } else {
-                        // if there is no user found with that facebook id, create them
-                        let newUser = {};
-
-                        // set all of the facebook information in our user model
-                        newUser.id = profile.id; // set the users facebook id
-                        newUser.token = token; // we will save the token that facebook provides to the user
-                        newUser.name = profile.displayName || 'custom nanme';
-                        // newUser.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-                        // newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-
-                        // save our user to the database
-                        Users.insert(newUser).then(function (newUs) {
-                            return done(null, newUs);
-                        });
-                    }
-
-                });
+                    })
+                    .catch(done);
             });
-
         }));
     // =========================================================================
     // FACEBOOK ================================================================
@@ -114,7 +98,6 @@ export default (passport) => {
                     })
                     .catch(done);
             });
-
         }));
 
 }
