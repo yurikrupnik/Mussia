1cff13ad17ebad723956a63dcb3c523a9f9c154a
yurikrupnik <krupnik.yuri@gmail.com>
2017-03-31 23:25:00 +0300

    added some reducers

---
+++ app/redux/actions/isFetching.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yurikrupnik on 31/03/2017.
+ */
--- app/redux/reducers/index.js
+++ app/redux/reducers/index.js
@@ -2,13 +2,15 @@ import counter from './counter';
 import counters from './counters';
 import payments from './payments';
 import user from './user';
+import errors from './errors';
 import {combineReducers} from 'redux';
 
 const reducers = {
     user,
     counters,
     counter,
-    payments
+    payments,
+    errors
 };
 
 export default combineReducers(reducers);
