e900c840ad3a976f2a1843af356d473647ff39ec
yurikrupnik <krupnik.yuri@gmail.com>
2017-04-01 04:39:57 +0300

    express router

--- app/api/payments/payment.model.js
+++ app/api/payments/payment.model.js
@@ -1,8 +1,4 @@
 import db from '../../config/db';
 let model = db.get('payments');
-let model2 = db.get('payment');
-console.log('model', model);
-console.log('model2', model2);
-
 export default model;
 
