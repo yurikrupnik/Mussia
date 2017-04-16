b58786572425eea7288de2693d1fba01340594ee
yurikrupnik <krupnik.yuri@gmail.com>
2017-04-16 18:10:37 +0300

    some redux actions improvements

--- app/redux/util.js
+++ app/redux/util.js
@@ -1,52 +1,79 @@
 import {bindActionCreators} from 'redux';
-
+import {has, get} from 'lodash';
 const REQUEST = 'REQUEST';
 const RECEIVE = 'RECEIVE';
 const ERROR = 'ERROR';
+const SEND = 'SEND';
+const DELETE = 'DELETE';
 
-const createActions = url => {
-    const URL = url.replace('/', '').toUpperCase();
-    return {
-        [`REQUEST_${URL}`]: `REQUEST_${URL}`,
-        [`RECEIVE_${URL}`]: `RECEIVE_${URL}`,
-        [`ERROR_${URL}`]: `ERROR_${URL}`,
-
-    };
-};
 const handleRequest = state => Object.assign({}, state, {isFetching: true});
-const handleReceive = (state, data) => Object.assign({}, state, {data, isFetching: false, isReceived: true});
+const handleReceive = (state, payload) => Object.assign({}, state, {data: payload, isFetching: false, isReceived: true});
 const handleError = state => Object.assign({}, state, {error: {fuck: true}});
+const handleSend = state => Object.assign({}, state, {error: {fuck: true}});
+const handleDelete = state => Object.assign({}, state, {error: {fuck: true}});
+
+const ACTIONS = [
+    {actionName: REQUEST, handler: handleRequest},
+    {actionName: RECEIVE, handler: handleReceive},
+    {actionName: ERROR, handler: handleError},
+    {actionName: SEND, handler: handleSend},
+    {actionName: DELETE, handler: handleDelete},
+];
+
+const createActionsByUrl = url => {
+    const URL = url.replace('/', '').toUpperCase();
+    return ACTIONS.reduce((current, next) => {
+        current[`${next.actionName}_${URL}`] = {
+            actionName: `${next}_${URL}`,
+            handler: next.handler // pass the handler as it is
+        };
+        return current;
+    }, {});
+    // return {
+    //     [`REQUEST_${URL}`]: `REQUEST_${URL}`,
+    //     [`RECEIVE_${URL}`]: `RECEIVE_${URL}`,
+    //     [`ERROR_${URL}`]: `ERROR_${URL}`,
+    //     [`SEND${URL}`]: `SEND_${URL}`,
+    //     [`DELETE${URL}`]: `DELETE_${URL}`,
+    //
+    // };
+};
+
 const createReducerByUrl = (initialState, url) => {
-    let actions = createActions(url);
+    let actions = createActionsByUrl(url);
     return (state = initialState, action) => {
-        let {type, data} = action;
-        if (actions.hasOwnProperty(action.type)) {
-            if (type.startsWith(REQUEST)) {
-                return handleRequest(state);
-            } else if (type.startsWith(RECEIVE)) {
-                return handleReceive(state, data);
-            } else if (type.startsWith(ERROR)) {
-                return handleError(state);
-            }
+        let {type, payload} = action;
+        if (actions.hasOwnProperty(type)) {
+            const currentAction = actions[type];
+            return currentAction.handler(state, payload);
         } else {
             return state;
         }
     }
 };
 const urlToUpper = str => str.replace('/', '').toUpperCase();
-const requestData = url => {
+
+const dispatchActionByPrefixAndUrl = (url, prefix) => {
     const URL = urlToUpper(url);
     return {
-        type: `${REQUEST}_${URL}`
+        type: `${prefix}_${URL}`,
+        payload: prefix
     };
 };
+
+const requestData = url => dispatchActionByPrefixAndUrl(url, REQUEST);
+
+const sendData = url => dispatchActionByPrefixAndUrl(url, SEND);
+
+const deleteData = url => dispatchActionByPrefixAndUrl(url, DELETE);
+
 const receivedData = url => dispatch => {
     const URL = urlToUpper(url);
     // async
     return (res = []) => { // todo see if need this , check server side
         return dispatch({
             type: `${RECEIVE}_${URL}`,
-            data: res.body || res // this is run on server and client, make sure to make those actions for async data
+            payload: res.body || res // this is run on server and client, make sure to make those actions for async data
         });
     };
 };
@@ -72,9 +99,7 @@ const createRead = Resource => (query, params) => dispatch => {
 };
 
 const createPost = Resource => (query, params) => dispatch => {
-    dispatch({
-        type: 'SEND'
-    });
+    dispatch(sendData(Resource.url));
     // console.log('query', query);
     // console.log('params', params);
     return Resource.post(params, query)
@@ -86,9 +111,10 @@ const createPost = Resource => (query, params) => dispatch => {
 };
 
 const createDelete = Resource => (query, params) => dispatch => {
-    dispatch({
-        type: 'DELETE'
-    });
+    // dispatch({
+    //     type: 'DELETE'
+    // });
+    dispatch(deleteData(Resource.url));
     // console.log('query', query);
     // console.log('params', params);
     return Resource.delete(params, query)
