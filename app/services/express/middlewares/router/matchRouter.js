9cf57975dd410fbc02ffbb91ed07d3fc61b3df6b
yurikrupnik <krupnik.yuri@gmail.com>
2017-04-21 02:41:22 +0300

    react router 4 and more npm updates

--- app/services/express/middlewares/router/matchRouter.js
+++ app/services/express/middlewares/router/matchRouter.js
@@ -8,6 +8,8 @@ import AppWrapper from '../../../../Wrappers/App';
 let preRenderString = (props) => renderToString(<AppWrapper {...props}/>);
 
 export default (req, res, next) => {
+    console.log('match', match);
+
     match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
         if (error) {
             res.status(500).send(error.message);
