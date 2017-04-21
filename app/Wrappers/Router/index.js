0dab0c40dfb005303afedcd7176bc0107acd9806
yurikrupnik <krupnik.yuri@gmail.com>
2017-04-21 17:42:27 +0300

    react router 4 moving on

--- app/Wrappers/Router/index.js
+++ app/Wrappers/Router/index.js
@@ -1,15 +1,59 @@
 // all routes will be wrapped in material provider
 import React from 'react';
-import {Router, browserHistory, RouterContext} from 'react-router';
+import PropTypes from 'prop-types';
+// import {Router, browserHistory, RouterContext} from 'react-router';
 import {routes} from '../../routes';
 import Material from '../Material';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    PrivateRoute,
+    withRouter,
+    Switch,
+    StaticRouter
+} from 'react-router-dom'
+
+
+
+import Root from '../../Wrappers/Root/Root'
+import Payments from '../../api/payments/component'
 
 export default (props) => {
     // handle server render and client render
     let RoutesWrapper = global.window ?
-        <Router history={browserHistory}>{routes}</Router> :
-        <RouterContext {...props} />;
+        (<Router>
+            <Switch>
+                <Route to="/payments" render={(val => {
+                    return (<Payments/>)
+                })} />
+            </Switch>
+        </Router>) :
+        <StaticRouter {...props} />;
+
     return (
-        <Material component={RoutesWrapper} />
+        <Material component={RoutesWrapper}/>
     )
 };
+// import { withRouter } from 'react-router'
+
+// A simple component that shows the pathname of the current location
+// class ShowTheLocation extends React.Component {
+//     static propTypes = {
+//         match: PropTypes.object.isRequired,
+//         location: PropTypes.object.isRequired,
+//         history: PropTypes.object.isRequired
+//     }
+//
+//     render() {
+//         const {match, location, history} = this.props
+//
+//         return (
+//             <div>You are now at {location.pathname}</div>
+//         )
+//     }
+// }
+//
+// // Create a new component that is "connected" (to borrow redux
+// // terminology) to the router.
+// export default withRouter(ShowTheLocation);
--- app/services/express/middlewares/router/matchRouter.js
+++ app/services/express/middlewares/router/matchRouter.js
@@ -1,25 +1,46 @@
 import React from 'react'
-import {match} from 'react-router';
-import {routes} from '../../../../routes';
+// import {matchPath} from 'react-router';
+// import {routes} from '../../../../routes';
 
 import {renderToString} from 'react-dom/server';
 import AppWrapper from '../../../../Wrappers/App';
-
-let preRenderString = (props) => renderToString(<AppWrapper {...props}/>);
+import { StaticRouter } from 'react-router'
+{/*let preRenderString = (props) => renderToString(<AppWrapper {...props}/>);*/}
 
 export default (req, res, next) => {
-    console.log('match', match);
+    const context = {};
 
-    match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
-        if (error) {
-            res.status(500).send(error.message);
-        } else if (redirectLocation) {
-            res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-        } else if (renderProps) {
-            res.locals.app = preRenderString(renderProps);
-            next();
-        } else {
-            res.redirect('/');
-        }
-    });
+
+    const html = renderToString(
+        <StaticRouter
+            location={req.url}
+            context={context}
+        >
+            <AppWrapper/>
+        </StaticRouter>
+    );
+
+    if (context.url) {
+        res.writeHead(301, {
+            Location: context.url
+        });
+        res.end()
+    } else {
+        res.locals.app = html;
+        next();
+    }
+    // console.log('match', matchPath);
+
+    // matchPath(req.url, (error, redirectLocation, renderProps) => {
+    //     if (error) {
+    //         res.status(500).send(error.message);
+    //     } else if (redirectLocation) {
+    //         res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    //     } else if (renderProps) {
+    //         res.locals.app = preRenderString(renderProps);
+    //         next();
+    //     } else {
+    //         res.redirect('/');
+    //     }
+    // });
 }
