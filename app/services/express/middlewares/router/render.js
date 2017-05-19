efde1700afb75bf13800fd94d08695c27d9b699c
yurikrupnik <krupnik.yuri@gmail.com>
2017-05-19 12:30:42 +0300

    / error

--- app/services/express/middlewares/router/render.js
+++ app/services/express/middlewares/router/render.js
@@ -1,5 +1,9 @@
 // render view template html - check views middleware
 export default (req, res) => {
+    if (req.url.match(/\//gi).length > 1) { // handle second slash in url error to load main.js todo-fix with webpack
+        res.redirect(`/${req.url.split('/')[1]}`);
+        return;
+    }
     res.status(200);
     res.render('index');
 }
