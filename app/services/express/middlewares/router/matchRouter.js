e4c6fceb83f3b75dfd5c42e0ce7119d42676922f
yurikrupnik <krupnik.yuri@gmail.com>
2017-05-20 14:09:24 +0300

    auth

--- app/services/express/middlewares/router/matchRouter.js
+++ app/services/express/middlewares/router/matchRouter.js
@@ -9,7 +9,6 @@ import { StaticRouter } from 'react-router-dom'
 
 export default (req, res, next) => {
     const context = {};
-    console.log('context', context);
     const html = renderToString(
         <StaticRouter
             location={req.url}
@@ -31,18 +30,4 @@ export default (req, res, next) => {
         res.locals.app = html;
         next();
     }
-    // console.log('match', matchPath);
-
-    // matchPath(req.url, (error, redirectLocation, renderProps) => {
-    //     if (error) {
-    //         res.status(500).send(error.message);
-    //     } else if (redirectLocation) {
-    //         res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-    //     } else if (renderProps) {
-    //         res.locals.app = preRenderString(renderProps);
-    //         next();
-    //     } else {
-    //         res.redirect('/');
-    //     }
-    // });
 }
