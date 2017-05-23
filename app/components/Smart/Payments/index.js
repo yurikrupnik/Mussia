8e4b72586d7449e7d76e4fedc5b8a2492214971d
yurikrupnik <krupnik.yuri@gmail.com>
2017-05-23 23:11:49 +0300

    ready for errors

--- app/components/Smart/Payments/index.js
+++ app/components/Smart/Payments/index.js
@@ -1,69 +1,17 @@
 import React, {Component} from 'react';
 // import PropTypes from 'prop-types';
-// import FlatButton from 'material-ui/FlatButton';
+import FlatButton from 'material-ui/FlatButton';
 // import resource from './request';
 import {
     BrowserRouter as Router,
-    Route,
     Link,
-    PrivateRoute,
-    withRouter,
-    Switch,
-    StaticRouter
 } from 'react-router-dom'
 
-import routess from './routes';
+import routes from './routes';
 import RouteWithSubRoutes from '../../Utils/RouteWithRoutes';
-import smartComponent from '../index';
-class PaymentsData extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-        // const {actions} = this.props;
-        const {actions, location, match} = this.props;
-        const {pathname, query, search} = location;
-        actions.read(query, {yalublu: true}, {
-            fields: ['name', 'info']
-        });
-    }
-
-    handleDelete(e, id){
-        const {actions, location, match} = this.props;
-        console.log('e', e);
-        console.log('id', id);
-
-        actions.delete({}, {}, [])
-    }
-
-    render() {
-        const payments = this.props[PaymentsData.selector];
-        const {match} = this.props;
-        const {data, count} = payments;
-        return (
-            <div> payments {
-                data.map(function (v, i) {
-                    return <div key={i}>
-                        <Link to={`${match.url}/${v._id}`}>{v.title}</Link>
-                    </div>
-                })
-            }</div>
-        )
-    }
-}
+// import smartComponent from '../index';
 
 
-const Payment = ({val, index}) => {
-    return (
-        <div className="payments">
-            <div key={index}>
-                <h2>{val.title}</h2>
-                <button >delete</button>
-            </div>
-        </div>
-    )
-};
 
 class Payments extends Component {
 
@@ -72,14 +20,14 @@ class Payments extends Component {
     }
 
 
-    componentDidMount() {
-        // const {actions} = this.props;
-        const {actions, location, match} = this.props;
-        const {pathname, query, search} = location;
-        actions.read(query, {yalublu: true}, {
-            fields: ['name', 'info']
-        });
-    }
+    // componentDidMount() {
+    //     // const {actions} = this.props;
+    //     const {actions, location, match} = this.props;
+    //     const {pathname, query, search} = location;
+    //     actions.read(query, {yalublu: true}, {
+    //         fields: ['name', 'info']
+    //     });
+    // }
     // handleDelete(id, e) {
     //     // console.log('e', e);
     //     //
@@ -90,7 +38,7 @@ class Payments extends Component {
     // }
 
     render() {
-        const {match, routes, } = this.props;
+        const {match} = this.props;
         // const payments = this.props[Payments.selector]; // smartComponent assigns selector
         // const {data, count} = payments;
         return (
@@ -99,10 +47,10 @@ class Payments extends Component {
                     <h5>Payments</h5>
                     <div>
                         <ul>
-                            <li><Link to={`${match.url}/create`}>create</Link></li>
+                            <li><FlatButton children={<Link to={`${match.url}/create`}>create</Link>} /></li>
                         </ul>
                     </div>
-                    {routess.map((route, i) => (
+                    {routes.map((route, i) => (
                         <RouteWithSubRoutes key={i} {...route}/>
                     ))}
                 </div>
@@ -112,4 +60,4 @@ class Payments extends Component {
     }
 }
 import request from '../../../api/payments/request';
-export default smartComponent(request, Payments);
+export default Payments;
