eafc5bf065c15f125ab625aaa9c70f07013c951b
yurikrupnik <krupnik.yuri@gmail.com>
2017-08-21 14:19:50 +0300

    photos api separate logic to controller

--- app/api/photos/controller.js
+++ app/api/photos/controller.js
@@ -1,4 +1,45 @@
 
-/**
- * Created by yurikrupnik on 17/08/2017.
- */
+import Model from '../galleries/model';
+
+const search = flickr => (req, res, next) => {
+    return flickr.photos.search({ tags: req.body.tag }, (_err, response) => {
+        if (_err) {
+            return next(_err);
+        }
+        let data = response.photos;
+        if (!req.isAuthenticated()) {
+            return res.status(200).json(data);
+        } else {
+            data.tag = req.body.tag;
+            data.user_id = req.user.length ? req.user[0].id : '';
+            let criteria = { user_id: data.user_id, tag: data.tag };
+
+            // todo promise it
+            Model.findOne(criteria, (err, item) => {
+                if (err) next(err);
+                if (item) res.status(200).json(item);
+                else {
+                    let newSearch = new Model(data);
+                    newSearch.save(function (er, doc) {
+                        if (er) next(er);
+                        return res.status(200).json(doc);
+                    });
+                }
+            })
+        }
+    });
+};
+
+const searchByPage = flickr => (req, res, next) => {
+    flickr.photos.search({ tags: req.body.tag, page: req.params.page }, (err, response) => {
+        if (err) return next(err);
+        let data = response.photos;
+        data.tag = req.body.tag;
+        res.status(200).json(data);
+    });
+};
+
+export {
+    search,
+    searchByPage
+}
--- app/api/photos/index.js
+++ app/api/photos/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import Flickr from 'flickrapi';
 import { url } from './config';
-import Model from '../galleries/model';
 import secret from '../../services/express/middlewares/passport/secrets.json';
-// import {} from './controller';
+import {search, searchByPage} from './controller';
 
 let router = express.Router();
 
@@ -15,43 +14,9 @@ const flickrOptions = {
 Flickr.tokenOnly(flickrOptions, function (error, flickr) {
     // we can now use "flickr" as our API object,
     // but we can only call public methods and access public data
-    router.post(url, (req, res, next) => {
-        return flickr.photos.search({ tags: req.body.tag }, (_err, response) => {
-            if (_err) {
-                return next(_err);
-            }
-            let data = response.photos;
-            if (!req.isAuthenticated()) {
-                return res.status(200).json(data);
-            } else {
-                data.tag = req.body.tag;
-                data.user_id = req.user.length ? req.user[0].id : '';
-                let criteria = { user_id: data.user_id, tag: data.tag };
+    router.post(url, search(flickr));
 
-                // todo promise it
-                Model.findOne(criteria, (err, item) => {
-                    if (err) next(err);
-                    if (item) res.status(200).json(item);
-                    else {
-                        let newSearch = new Model(data);
-                        newSearch.save(function (er, doc) {
-                            if (er) next(er);
-                            return res.status(200).json(doc);
-                        });
-                    }
-                })
-            }
-        });
-    }); // todo move handler to controller
-
-    router.post(`${url}/:page`, function (req, res, next) {
-        flickr.photos.search({ tags: req.body.tag, page: req.params.page }, (err, response) => {
-            if (err) return next(err);
-            let data = response.photos;
-            data.tag = req.body.tag;
-            res.status(200).json(data);
-        });
-    }); // todo move handler to controller
+    router.post(`${url}/:page`, searchByPage(flickr));
 
 });
 
