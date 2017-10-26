b2ea51082138c9d4502b5d1c8e2d992f2eb8062e
yurikrupnik <krupnik.yuri@gmail.com>
2017-10-27 01:31:57 +0300

    Mouse example

--- app/components/views/admin/index.js
+++ app/components/views/admin/index.js
@@ -6,6 +6,34 @@ import {connect} from 'react-redux';
 import routes from './routes';
 import withRoutes from '../../HOC/withSubRoutes';
 
+
+const withMouse = function (WrappedComponent) {
+    return class extends Component {
+        constructor(props) {
+            super(props);
+            // this.state = {x: 0, y: 0};
+            this.handleMouseMove = this.handleMouseMove.bind(this);
+        }
+
+        state = {x: 0, y: 0};
+
+        handleMouseMove(event) {
+            this.setState({
+                x: event.clientX,
+                y: event.clientY
+            });
+        }
+
+        render() {
+            return (
+                <div style={{height: '100%'}} onMouseMove={this.handleMouseMove.bind(this)}>
+                    <WrappedComponent {...this.state}/>
+                </div>
+            )
+        }
+    }
+};
+
 class Mouse extends Component {
     constructor(props) {
         super(props);
@@ -20,9 +48,6 @@ class Mouse extends Component {
     }
 
     render() {
-
-        console.log('this.props.children', this.props.children);
-
         return (
             <div style={{height: '100%'}} onMouseMove={this.handleMouseMove.bind(this)}>
                 {this.props.children(this.state)}
@@ -31,10 +56,28 @@ class Mouse extends Component {
     }
 }
 
+const Ass = () => <Mouse>
+    {mouse => {
+        return <MousePosition x={mouse.x} y={mouse.y}/>
+    }}
+</Mouse>;
+
+const MousePosition = ({x, y}) => {
+    return (
+        <div style={{height: '100%'}}>
+            <div style={{height: '100%'}}>
+                <h1>The mouse position is ({x}, {y})</h1>
+            </div>
+        </div>
+    )
+};
 
 class Admin extends Component {
 
     render() {
+
+        const StaticMouse = withMouse(MousePosition);
+
         return (
             <div style={{height: '100%'}}>
                 <h2>Admin</h2>
@@ -45,17 +88,13 @@ class Admin extends Component {
                         </Link>
                     </li>
                 </ul>
+                <StaticMouse/>
                 <Mouse>
-                    {(mouse) => {
-                        return (
-                            <div style={{height: '100%'}}>
-                                <div style={{height: '100%'}}>
-                                    <h1>The mouse position is ({mouse.x}, {mouse.y})</h1>
-                                </div>
-                            </div>
-                        )
+                    {mouse => {
+                        return <MousePosition x={mouse.x} y={mouse.y}/>
                     }}
                 </Mouse>
+                <Ass />
             </div>
         )
     }
